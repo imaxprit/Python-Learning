43a0fcd62c47b698adc89cdbbacc4473c060c1e3
arpit gupta <rajarpitgupta@gmail.com>
2026-06-04 20:08:52 +0530

    dataTypes

---
+++ Basics/dataTypes.py
@@ -0,0 +1,50 @@
+# python data types
+
+# numeric data types
+# integer, float and boolean
+
+a = 10
+b = 25.86
+c = 2 + 4j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# sequence dataTypes
+
+# String
+s = "Hello Python Developer"
+# print(s)
+# print(type(s))
+
+# accessing string with index
+# print(s[1])
+# print(s[-1])
+
+# List
+li = [1, 2, 3, "Arpit", 78.96, 3]
+# print(li)
+li[1] = 15
+# print(li)
+
+# tuples
+tp = ('GFG', 58.6, 45, 60, 45)
+# print(tp)
+tp = ('GFG', 58, 45, 60)
+# print(tp)
+
+# Boolean DataTypes
+
+# print(type(True))
+# print(type(False))
+
+# Set data types
+
+s1 = {"a", "b", "c", "d", 78, 56}
+# print(s1)
+
+# dictionary dataTypes
+
+d = {1:"Name", 2: "City", 3 : "Phone"}
+print(d[1])
+print(d.get(4))
