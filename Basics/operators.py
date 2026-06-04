7d44b773c1d340b683bb6d76a62aeb0e5e029ca5
arpit gupta <rajarpitgupta@gmail.com>
2026-06-04 19:39:18 +0530

    operators

---
+++ Basics/operators.py
@@ -0,0 +1,14 @@
+# python operators
+
+# Arithmetic Operators
+
+a = 10
+b = 3
+
+print("Addition = ", (a+b))
+print("Subtraction = ", (a-b))
+print("Multiplication = ", (a*b))
+print("Division = ", (a/b))
+print("Floor Div = ", (a//b))
+print("Modulus = ", (a%b))
+print("Power = ", (a ** b))
