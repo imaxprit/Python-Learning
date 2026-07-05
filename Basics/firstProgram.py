f53c46644b6d3e86db86bd3daa35696e9c5d6140
arpit gupta <rajarpitgupta@gmail.com>
2026-07-05 20:47:38 +0530

    myfile

--- Basics/firstProgram.py
+++ Basics/firstProgram.py
@@ -1,17 +1,22 @@
-# print hello world
+# # print hello world
 
-print("Hello World!")
-print("Welcome to Microsoft")
-print("Your are a new Subscriber")
+# print("Hello World!")
+# print("Welcome to Microsoft")
+# print("Your are a new Subscriber")
 
-""" this is 
-python multi lint 
-comment
-"""
+# """ this is 
+# python multi lint 
+# comment
+# """
 
-#sample comments
-name = "arpitgupta"
-print("Hello",name)
+# #sample comments
+# name = "arpitgupta"
+# print("Hello",name)
 
-print("Avengers")
-print("Spiderman : No way home");
+# print("Avengers")
+# print("Spiderman : No way home");
+
+# --------------------------------------------------------------------------------
+
+print("My name is Arpit");
+print("Welcome to Python Learning");
