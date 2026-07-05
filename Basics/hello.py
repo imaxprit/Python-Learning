3fa66eb219c99213d4c99a28e1249c824f1b01bf
arpit gupta <rajarpitgupta@gmail.com>
2026-07-05 21:16:53 +0530

    added changes

--- Basics/hello.py
+++ Basics/hello.py
@@ -34,3 +34,26 @@ age = "21"
 # print("My name is", name, "and I am", age, "years old.")
 # print("My name is "+name+" and I am "+ age + " years old.")
 
+x = 15
+y = 20
+
+print("The sum of", x, "and", y, "is = ",(x+y))
+
+
+name = "BOB"
+age = 30
+print(f"My name is {name} and I am {age} years old")
+
+
+m = 10
+n = 20
+o = 30
+print(m,n,o, sep="-")
+
+my_var = 456
+print(f"Your pass is {my_var}")
+my_var = 569
+print("No! this is your pass",my_var)
+my_var = "School"
+print("The correct pass is " + my_var)
+
