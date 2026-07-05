d8ebb194a8d60bd6e1afcd3a3ea60dd6380968ed
arpit gupta <rajarpitgupta@gmail.com>
2026-07-05 21:06:43 +0530

    printStatement

--- Basics/hello.py
+++ Basics/hello.py
@@ -12,3 +12,25 @@ print("Course : MCA")
 print("Semester : 3rd")
 print("Address : Lucknow")"""
 
+# Assigning values to variables 
+
+x = 10
+name = "Alice"
+pi = 3.14
+# print(x)
+# print(name)
+# print(pi)
+
+# first_name = "Peter"
+# last_name = "Parker"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+x, y, z = 1, 2, 3
+# print(x, y, z)
+
+name = "Arpit"
+age = "21"
+# print("My name is", name, "and I am", age, "years old.")
+# print("My name is "+name+" and I am "+ age + " years old.")
+
