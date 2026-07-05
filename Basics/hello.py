4994462f0f63a83ba1ed4c587a107451fe94b6c4
arpit gupta <rajarpitgupta@gmail.com>
2026-07-05 20:57:25 +0530

    filePy

--- Basics/hello.py
+++ Basics/hello.py
@@ -3,3 +3,12 @@ print("Welcome to, \nPython Learning");
 
 print("Hello,", end=" ")
 print("Arpit")
+
+# Print Student Details
+print("--------")
+
+"""print("Name : Arpit Gupta")
+print("Course : MCA")
+print("Semester : 3rd")
+print("Address : Lucknow")"""
+
