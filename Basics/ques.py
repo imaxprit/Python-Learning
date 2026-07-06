3a3ec6372614044db0bdedcd574d44122854ac42
arpit gupta <rajarpitgupta@gmail.com>
2026-07-06 19:55:20 +0530

    quesPy

---
+++ Basics/ques.py
@@ -0,0 +1,22 @@
+# Take the radius from the user and display the volume of sphere
+
+# radius = int(input("Enter radius : "))
+# rad_cube = radius**3
+# vol_sphere = 4/3 * 3.14 * rad_cube
+
+# print("Volume of sphere is", round(vol_sphere, 3))
+
+# Take the principal, rate and time from the user and display the simple interest
+
+# p = int(input("Enter principal : "))
+# r = int(input("Enter rate : "))
+# t = int(input("Enter time : "))
+# si = (p * r * t) / 100
+# print("Simple Interest is", si)
+
+sol_exp = 3 + 2 ** 4 / 2 * 5 - 8 // 2
+
+# 3 + 16 / 2 * 5 - 8 // 2
+# 3 + 40 - 4
+# 3 + 36
+print(sol_exp)
