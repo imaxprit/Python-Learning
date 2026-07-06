83a570f1b03642764e7dca73ec8a97b835d524ba
arpit gupta <rajarpitgupta@gmail.com>
2026-07-06 19:43:45 +0530

    operators

--- Basics/operators.py
+++ Basics/operators.py
@@ -2,15 +2,51 @@
 
 # Arithmetic Operators
 
-a = 10
-b = 3
+# a = int(input("Enter num1 : "))
+# b = int(input("Enter num2 : "))
 
-print("Addition = ", (a+b))
-print("Subtraction = ", (a-b))
-print("Multiplication = ", (a*b))
-print("Division = ", (a/b))
-print("Floor Div = ", (a//b))
-print("Modulus = ", (a%b))
-print("Power = ", (a ** b))
+# print("Addition = ", (a+b))
+# print("Subtraction = ", (a-b))
+# print("Multiplication = ", (a*b))
+# print("Division = ", (a/b))
+# print("Floor Div = ", (a//b))
+# print("Modulus = ", (a%b))
+# print("Power = ", (a ** b))
 
-print("All values are using by Operators")
+# print("All values are using by Operators")
+
+# Assignment Operators
+
+# print(a, b)
+# a += b
+# b -= a
+# print(a, b)
+
+# Comparison Operators
+
+# print(a, "==", b, "=", (a==b))
+# print(a, ">=", b, "=", (a>=b))
+# print(a, "<=", b, "=", (a<=b))
+# print(a, "!=", b, "=", (a!=b))
+
+# Logical Operators
+
+# print((a > b) and (b > 12))
+# print((a == b) or (a < b))
+# print(not(a == b))
+
+# Membership Operators
+
+fruits = ["Apple", "Mango", "Banana"]
+# print("Apple" in fruits)
+# print("Peers" in fruits)
+# print("Guava" not in fruits)
+# print("Banana" not in fruits)
+
+# Identity Operators
+
+x = "Home"
+y = "home"
+# print(x is y)
+# print(x is not y)
+
