5d6bd49c738a084d4cef25a53a8f0520bdf7b95c
arpit gupta <rajarpitgupta@gmail.com>
2026-07-07 19:39:33 +0530

    conditionalState

---
+++ Basics/controlStatement.py
@@ -0,0 +1,23 @@
+#  Conditional Statements
+
+# age = 21
+# if age > 18 :
+#     print("You ara Adult")
+
+# marks = 35
+# if marks > 50 :
+#     print("Pass")
+# else :
+#     print("Fail")
+
+
+result = int(input("Enter your result score : "))
+if result > 85 :
+    print("You scored grade A")
+elif result > 70 :
+    print("You scored grade B")
+elif result > 55 :
+    print("You scored grade C")
+else :
+    print("better luck next time")
+
