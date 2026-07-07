7f05d1160becd296ccd246854a26a2f4e6da9fc0
arpit gupta <rajarpitgupta@gmail.com>
2026-07-07 19:55:27 +0530

    ques

--- Basics/ques.py
+++ Basics/ques.py
@@ -19,17 +19,17 @@ sol_exp = 3 + 2 ** 4 / 2 * 5 - 8 // 2
 # 3 + 16 / 2 * 5 - 8 // 2
 # 3 + 40 - 4
 # 3 + 36
-print(sol_exp)
+# print(sol_exp)
 
 # type casting 
 
 num = 42
-print(type(num))
-print(type(str(num)))
+# print(type(num))
+# print(type(str(num)))
 
 name = "123"
-print(type(name))
-print(type(int(name)))
+# print(type(name))
+# print(type(int(name)))
 
 # price = 425.76
 # print(type(price))
@@ -45,7 +45,7 @@ print(type(int(name)))
 
 x = 10
 y = 5
-print(x > y and x < 15)
+# print(x > y and x < 15)
 
 x = 5
 y = "2"
@@ -54,4 +54,42 @@ y = "2"
 sol2_exp = 8 // 3 + 4 % 2
 
 # 2 + 2 = 4.0
-print(sol2_exp)
+# print(sol2_exp)
+
+# Take an integer input and tell if it is positive or negative (zero not included)
+
+num = int(input("Enter an integer : "))
+
+# if num > 0 :
+#     print(f"{num} is Positive")
+# else :
+#     print(f"{num} is Negative")
+
+#  Take a positive integer input and tell if it is even or odd
+
+# if num > 0 :
+#     if (num%2 == 0) :
+#         print(f"{num} is Even Number")
+#     else :
+#         print(f"{num} is Odd Number")
+# else :
+#     print("Please enter a positive(+) number")
+
+
+
+# If cost price and selling price of an item is input through the keyboard, write a program to determine 
+# whether the seller has made profit or incurred loss or no profit no loss. Also determine how much profit he 
+# made or loss he incurred.
+
+cost_price = float(input("Enter cost price = "))
+selling_price = float(input("Enter selling price = "))
+
+profit = selling_price - cost_price
+profit_percentage = (profit / cost_price) * 100
+
+if profit > 0 :
+    print(f"Seller has made profit of {profit} units, And percentage is {round(profit_percentage, 2)}%")
+elif profit < 0 :
+    print(f"Seller has incurred loss of {profit} units, And percentage is {round(profit_percentage, 2)}%")
+else :
+    print("Neither loss or Profit")
