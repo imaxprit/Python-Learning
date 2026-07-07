bf2152d086894422f7109fc207051c53b88e7286
arpit gupta <rajarpitgupta@gmail.com>
2026-07-07 20:38:22 +0530

    match case

--- Basics/controlStatement.py
+++ Basics/controlStatement.py
@@ -11,13 +11,37 @@
 #     print("Fail")
 
 
-result = int(input("Enter your result score : "))
-if result > 85 :
-    print("You scored grade A")
-elif result > 70 :
-    print("You scored grade B")
-elif result > 55 :
-    print("You scored grade C")
-else :
-    print("better luck next time")
+# result = int(input("Enter your result score : "))
+# if result > 85 :
+#     print("You scored grade A")
+# elif result > 70 :
+#     print("You scored grade B")
+# elif result > 55 :
+#     print("You scored grade C")
+# else :
+#     print("better luck next time")
 
+
+# Match Case
+
+def day_example(value) :
+    match value :
+        case 1 :
+            print("Monday")
+        case 2 :
+            print("Tuesday")
+        case 3 :
+            print("Wednesday")
+        case 4 :
+            print("Thursday")
+        case 5 :
+            print("Friday")
+        case 6 :
+            print("Saturday")
+        case 7 :
+            print("Sunday")
+day_example(1)
+day_example(2)
+day_example(6)
+day_example(3)
+day_example(5)
