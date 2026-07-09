9b4d44e3707c8b4ecfcaff22fb3c3c4706ed8bbe
arpit gupta <rajarpitgupta@gmail.com>
2026-07-09 19:38:52 +0530

    whileLoop

---
+++ Basics/loops2.py
@@ -0,0 +1,41 @@
+# while loop
+
+# n = 1
+# while (n <= 5) :
+#     print(n, "Hello")
+#     n += 1
+# print("Welcome")
+
+# Print numbers from 1 to 100
+
+# i = 1
+# while i <= 100 :
+#     print(i, end=" ")
+#     i += 2
+# print("Printed Hundread Times!")
+
+
+# j = 0
+# while j <= 10:
+#     print(j, end=" ")
+#     j += 1
+
+
+# x = 1
+# while x == 1:
+#     x = x -1
+#     print(x)
+
+# i = 10
+# while i==20:
+#     print("computer buff")
+
+x = 4
+y = 0
+while x >= 0:
+    x -= 1
+    y += 1
+    if x==y :
+        continue
+    else :
+        print(x, y)
