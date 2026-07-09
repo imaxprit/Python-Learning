eaa61652427dc35d42da5d7727d30f508ade964f
arpit gupta <rajarpitgupta@gmail.com>
2026-07-09 19:57:43 +0530

    NumPattern

--- Basics/patternQues.py
+++ Basics/patternQues.py
@@ -19,9 +19,18 @@
 #     print()
 
 
-# print star pattern
+
 
 n = int(input("Enter n : "))
-for i in range(1, n+1) :
-    print("*" * i)
-print()
+
+# print star pattern
+# for i in range(1, n+1) :
+#     print("*" * i)
+# print()
+
+# print number pattern
+for i in range(1, n+1):
+    for j in range(1, i+1) :
+        print(j, end=" ")
+    print()
+
