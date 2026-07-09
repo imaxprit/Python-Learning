26d50bfaf84fa772c95d91923ac84845f9111332
arpit gupta <rajarpitgupta@gmail.com>
2026-07-09 19:47:25 +0530

    pattern

---
+++ Basics/patternQues.py
@@ -0,0 +1,19 @@
+# Pattern Printing Questions
+
+# For n = 1
+# *****
+
+# rows = 1
+# n = 5
+# while rows <=5 :
+#     print("*", end=" ")
+#     rows += 1
+
+n = int(input("Enter n : "))
+# for i in range(n) :
+#     print("*" * 5)
+
+for i in range(n) :
+    for j in range(1, n+1) :
+        print(j, end=" ")
+    print()
