e28fbaeed8d2281ddba6875b047f60f30470fea4
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-10 20:22:58 +0530

    ques6to10

--- Basics/PracticeQues.py
+++ Basics/PracticeQues.py
@@ -31,3 +31,42 @@
 #     print(f"{num} x {i} = {num*i}")
 # print("Table of", num, "printed.")
 
+# 6. Calculate the cube of all numbers from 1 to a given number
+
+# given_number = int(input("Enter number :"))
+# for i in range(1, given_number+1):
+#     print(f"Current Number is : {i} and the cube is {i**3}")
+
+# 7. Display numbers from a list using a loop
+
+# numbers = [12, 75, 150, 180, 145, 525, 50]
+# for num in numbers :    
+#     if (num > 500) :
+#         break
+#     if (num > 150) :
+#         continue
+#     if (num%5 == 0) :
+#         print(num)
+
+# 8. Count occurrences of a specific element in a list
+
+# list1 = [10, 20, 10, 30, 40, 10, 50, 20]
+# target = 20
+# count = 0
+# for num in list1:
+#     if(target == num) :
+#         count += 1
+# print(f"{target} appears {count} times")
+
+# 9. Print elements from a list present at odd index positions
+
+# my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# for i in range(1, len(my_list), 2) :
+#         print(my_list[i], end=" ")
+
+
+# 10. Print list in reverse order using a loop
+list1 = [10, 20, 30, 40, 50]
+for num in reversed(list1):
+        print(num)
+
