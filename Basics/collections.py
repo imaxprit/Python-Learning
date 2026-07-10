4ecc4989fd310fe0c364a1daacde78ec2fe29530
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-10 20:31:12 +0530

    pythonArrays

---
+++ Basics/collections.py
@@ -0,0 +1,20 @@
+# Python Collections(Arrays)
+
+# This collection in python contains 4 built in datatypes:
+# 1. Lists
+# 2. Tuple
+# 3. Set
+# 4. Dictionary
+
+# List
+
+my_list = ["apple", "banana", "cherry"]
+print(my_list)
+
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[-2])
+# print(my_list[-1])
+
+my_list[1] = "mango"
+print(my_list)
