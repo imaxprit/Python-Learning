cf68aff4283260a3b57b075b9500ea19ae592d07
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-10 19:57:02 +0530

    ques1-5

---
+++ Basics/PracticeQues.py
@@ -0,0 +1,33 @@
+# 1. Print first 10 natural numbers using while loop
+
+# n = 1
+# while n <= 10:
+#     print(n, end=" ")
+#     n += 1
+
+# 2. Display numbers from -10 to -1 using for loop
+
+# for i in range(-10, 0):
+#     print(i)
+
+# 3. Display a message “Done” after successful execution of for loop
+
+# for i in range(0, 5):
+#     print(i)
+# print("Done")
+
+# 4. Calculate the sum of all numbers from 1 to N
+
+# n = 10
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+# print(sum)
+
+# 5. Print multiplication table of a given number
+
+# num = int(input("Enter Table Number : "))
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num*i}")
+# print("Table of", num, "printed.")
+
