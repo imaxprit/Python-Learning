ee248fbd5e9b4d4168cfbdd06f6e6057f84aa408
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-11 18:28:08 +0530

    sorting

--- Basics/Lists.py
+++ Basics/Lists.py
@@ -21,3 +21,38 @@ myList = ["Dog", "Cat", "Fish"]
 # nums.extend(myList)
 # print(myList)
 # print(nums)
+
+# Removing elements from a list
+
+# myList.remove("Fish")
+# print(myList)
+# myList.pop(0)
+# myList.pop(0)
+# print(myList)
+
+# Changing item in a list
+
+myList[0] = "Elephant"
+# print(myList)
+
+myList[1:3] = ["Girraf", "Fox"]
+# print(myList)
+
+# Sorting List
+
+nums = [1, 7, 8, 4, 6, 2, 9, 3]
+nums.sort()
+# print(nums)
+
+char = ["Ball", "Ant", "Ice", "Div"]
+char.sort()
+print(char)
+
+nums.sort(reverse=True)
+# print(nums)
+
+nums.reverse()
+# print(nums)
+
+newList = [x for x in char if "a" in x]
+print(newList)
