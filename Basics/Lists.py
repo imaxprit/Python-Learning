367a4912828f1c5eaf630fa366d3d48ba2253266
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-11 18:13:02 +0530

    adding items

---
+++ Basics/Lists.py
@@ -0,0 +1,23 @@
+# Accessing Items
+
+# Indexing
+
+myList = ["Dog", "Cat", "Fish"]
+
+# print(myList)
+# print(myList[0])
+# print(myList[-1])
+# print(myList[1:3])
+# print(myList[:])
+# print(myList[-2:-1])
+
+# Adding items to a list
+
+# myList.append("Horse")
+# myList.insert(1, "Cow")
+
+# nums = [2, 4, 6]
+# myList.extend(nums)
+# nums.extend(myList)
+# print(myList)
+# print(nums)
