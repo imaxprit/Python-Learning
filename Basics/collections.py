4765f0aa75132d1c561e2bfa27e14648f3edfef0
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-11 18:02:32 +0530

    lists

--- Basics/collections.py
+++ Basics/collections.py
@@ -9,7 +9,7 @@
 # List
 
 my_list = ["apple", "banana", "cherry"]
-print(my_list)
+# print(my_list)
 
 # print(my_list[0])
 # print(my_list[1])
@@ -17,4 +17,42 @@ print(my_list)
 # print(my_list[-1])
 
 my_list[1] = "mango"
-print(my_list)
+# print(my_list)
+
+
+colors = ["Red", "Blue", "Green", "Yellow"]
+# print(colors)
+# print(type(colors))
+
+num = [10, 12, 15, 18, 21]
+# print(num)
+# print(type(num))
+
+result = [78.5, 89.9, 68.7, 86.4]
+# print(result)
+
+info = ["Arpit", 71, 85.7, True]
+info = [14, "Harshit", 71, 85.7, 14, "Harshit"]
+# print(info)
+# print(info[0])
+
+# for i in info:
+#     print(i)
+
+# print(type(info))
+
+# print(len(info))
+
+newList = list(("amazon", "google", "meta"))
+# print(newList)
+# print(type(newList))
+
+# taking input for a list:
+
+# this_list = []
+# n = 4
+# for i in range(n) :
+#     element = int(input("enter element : "))
+#     this_list.append(element)
+# print(this_list)
+
