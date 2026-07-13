ba0e40012b6d23cc188e2469fa7fc4e60c79e9e9
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-13 20:31:12 +0530

    setsQues

--- Basics/ques.py
+++ Basics/ques.py
@@ -226,13 +226,42 @@ def food_time(value) :
 
 
 
-def Reverse(alpha) :
-    list = []
-    for k in reversed(alpha) :
-        list = list + [k]
-    tuple(list)
-    print(list)
+# def Reverse(alpha) :
+#     list = []
+#     for k in reversed(alpha) :
+#         list = list + [k]
+#     tuple(list)
+#     print(list)
 
-alpha = (14, 12, 10, 9, 8)
-Reverse(alpha)
-print(type(alpha))
+# alpha = (14, 12, 10, 9, 8)
+# Reverse(alpha)
+# print(type(alpha))
+
+
+# sets = ([8, 16, 24, 1, 25, 3, 10, 65, 55])
+# #  Print the maximum and minimum element in a set in Python
+# def MIN(sets) :
+#     return (min(sets))
+# def MAX(sets) :
+#     return (max(sets))
+
+# print(MIN(sets))
+# print(MAX(sets))
+
+#  Given three arrays, we have to find common elements in three sorted lists using sets.
+
+arr1 = [1, 5, 10, 20, 40, 80]
+arr2 = [6, 7, 20, 80, 100]
+arr3 = [3, 4, 15, 20, 30, 70, 80, 120]
+
+def InterSectionOfSets(arr1, arr2, arr3) :
+    s1 = set(arr1)
+    s2 = set(arr2)
+    s3 = set(arr3)
+
+    set1 = s1.intersection(s2)
+    result = set1.intersection(s3)
+
+    final_list = list(result)
+    print(final_list)
+InterSectionOfSets(arr1, arr2, arr3)
