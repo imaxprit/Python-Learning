efb19df8196acef7793b1d017bd1c8dfcb29f15e
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-14 19:55:14 +0530

    ques

--- Basics/ques.py
+++ Basics/ques.py
@@ -254,14 +254,22 @@ arr1 = [1, 5, 10, 20, 40, 80]
 arr2 = [6, 7, 20, 80, 100]
 arr3 = [3, 4, 15, 20, 30, 70, 80, 120]
 
-def InterSectionOfSets(arr1, arr2, arr3) :
-    s1 = set(arr1)
-    s2 = set(arr2)
-    s3 = set(arr3)
+# def InterSectionOfSets(arr1, arr2, arr3) :
+#     s1 = set(arr1)
+#     s2 = set(arr2)
+#     s3 = set(arr3)
 
-    set1 = s1.intersection(s2)
-    result = set1.intersection(s3)
+#     set1 = s1.intersection(s2)
+#     result = set1.intersection(s3)
 
-    final_list = list(result)
-    print(final_list)
-InterSectionOfSets(arr1, arr2, arr3)
+#     final_list = list(result)
+#     print(final_list)
+# InterSectionOfSets(arr1, arr2, arr3)
+
+
+# sum of all items in the dictionary
+
+myDict = {'a' : 100, 'b':200, 'c':300}
+myDict2 = {'x': 25, 'y': 18, 'z': 45}
+print(sum(myDict.values()))
+print(sum(myDict2.values()))
