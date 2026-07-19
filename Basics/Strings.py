bcb8f9830e1381f6cfece05e731d73890acde41d
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-19 17:31:28 +0530

    Traversing a String

--- Basics/Strings.py
+++ Basics/Strings.py
@@ -3,9 +3,39 @@
 name = 'Arav'
 message = "Good Evening"
 greet = '''Hello,'''
-print(greet, name, message)
+# print(greet, name, message)
 
 # print(name[0])
 # print(name[2])
 # print(name[3])
 # print(name[-1])
+
+# Traversing a string
+
+subject = "Computer"
+
+# print(subject)
+# print(len(subject))
+
+# Using a for loop
+
+# for i in subject:
+#     print(i, end="-")
+
+# Using a while loop
+
+# i = 0
+# while i < len(subject) :
+#     print(subject[i], end=" ")
+#     i+=1
+
+# Using list comprehension 
+
+# charcters = [char for char in subject]
+# print(charcters)
+
+# Using Enumerate for Both Index and Character
+
+# for index, char in enumerate(subject) :
+#     print(f"Character at index {index}: {char}")
+
