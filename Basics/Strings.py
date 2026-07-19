2d26dbab048201c93061b69d0a3224f83ad20914
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-19 17:22:03 +0530

    String in Python

---
+++ Basics/Strings.py
@@ -0,0 +1,11 @@
+# Python Strings
+
+name = 'Arav'
+message = "Good Evening"
+greet = '''Hello,'''
+print(greet, name, message)
+
+# print(name[0])
+# print(name[2])
+# print(name[3])
+# print(name[-1])
