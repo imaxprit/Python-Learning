fba868f6bc417a04b08183c527effa9ee694590c
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-22 18:48:55 +0530

    Ques 1 Solved

---
+++ Basic Exercise/pythonExercise.py
@@ -0,0 +1,12 @@
+# Exercise 1. Arithmetic Product and Conditional Logic
+
+def logic_calc(num1, num2) :
+    product = num1 * num2
+    if product <= 1000:
+        print("The result is", product)
+    else :
+        print("The result is", num1+num2)
+
+logic_calc(20,30)
+logic_calc(40, 30)
+     
