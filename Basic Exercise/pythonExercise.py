27c0a10fa7c8c403767f69f899bdfd6aabf92df9
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-07-22 18:56:42 +0530

    Ques 2 Solved

--- Basic Exercise/pythonExercise.py
+++ Basic Exercise/pythonExercise.py
@@ -7,6 +7,18 @@ def logic_calc(num1, num2) :
     else :
         print("The result is", num1+num2)
 
-logic_calc(20,30)
-logic_calc(40, 30)
+# logic_calc(20,30)
+# logic_calc(40, 30)
+
+# Exercise 2. Cumulative Sum of a Range
+
+def print_range(num):
+    prev_num = 0
+    for i in range(num):
+        sum = prev_num + i
+        print(f"Current Number {i} Previous Number {prev_num} Sum: {sum}")
+        prev_num = i
+
+print_range(10)
+    
      
