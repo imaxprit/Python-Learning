94f995348a372917f1962c562b3660ad842f1bc6
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-19 09:43:28 +0530

    Count Letters

---
+++ Loops Exercise/CountLetters.py
@@ -0,0 +1,18 @@
+# Exercise 12. Count vowels and consonants in a sentence
+
+
+sentence = "Loops are fun!"
+vowels = "aeiou"
+v_count = 0
+c_count = 0
+
+for char in sentence.lower():
+    if char.isalpha():
+        if char in vowels:
+            v_count+=1
+        else :
+            c_count+=1
+
+
+print(f"Vowels: {v_count}")
+print(f"Consonents: {c_count}")
