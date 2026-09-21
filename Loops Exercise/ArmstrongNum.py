e8640958a812d660252fe69b7d11b342861a8640
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-21 12:27:26 +0530

    Armstrong Number

---
+++ Loops Exercise/ArmstrongNum.py
@@ -0,0 +1,14 @@
+# Exercise 19. Armstrong Number Check
+
+num = 145
+num_str = str(num)
+power = len(num_str)
+total = 0
+
+for digit in num_str:
+    total += int(digit) ** power
+
+if total == num:
+    print(f"{num} is an Armstrong number")
+else:
+    print(f"{num} is not an Armstrong number")
