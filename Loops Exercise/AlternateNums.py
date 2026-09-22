3c2aa008e4dbf9c4caf316bb081e7e99c65f961a
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-22 09:57:56 +0530

    Alternate Numbers

---
+++ Loops Exercise/AlternateNums.py
@@ -0,0 +1,15 @@
+# Exercise 22. Print the alternate numbers pattern
+
+n = 20
+
+for i in range(1, n+1, 2):
+    print(i, end=" ")
+print()
+
+
+# using while loop
+j = 1
+while j <= n:
+    print(j, end=" ")
+    j+=2
+print() 
