a905607e60f19c96015ca9d5e943386cbd62e24c
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-23 10:32:19 +0530

    Hollow Square

---
+++ Loops Exercise/HollowSqr.py
@@ -0,0 +1,11 @@
+# Exercise 24. Hollow square pattern
+
+size = 5
+
+for i in range(size):
+    for j in range(size):
+        if i == 0 or i == size-1 or j == 0 or j == size-1:
+            print("*", end=" ")
+        else :
+            print(" ", end=" ") 
+    print()
