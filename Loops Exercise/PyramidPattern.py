37b9c1d7e2fd080996e62284ee303a4337242316
arpit gupta <223596306+imaxprit@users.noreply.github.com>
2026-09-23 10:43:12 +0530

    Pyramid Star Pattern

---
+++ Loops Exercise/PyramidPattern.py
@@ -0,0 +1,13 @@
+# Exercise 25. Print pyramid pattern of stars
+
+rows = 5
+
+for i in range(0, rows):
+    for j in range(0, i+1):
+        print("*", end=" ")
+    print()
+
+for i in range(rows-1, 0, -1):
+    for j in range(0, i):
+        print("*", end=" ")
+    print()
